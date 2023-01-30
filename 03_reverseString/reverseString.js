e11c15688186a1175f3ae0d16fa36f6cd5604c61
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-01-30 17:17:55 +0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+/**
+ * Return its input, reversed!
+ * @param {string} text 
+ * @returns {string} reversed string.
+ */
+const reverseString = function(text) {
+    let result = "";
 
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
