86f93875f3b51265af1d81fcf0b999a6af2e047c
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-01-30 18:45:06 +0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+/**
+ * Removes the specified numbers from array.
+ * @param {Array} array 
+ * @param {...*} items
+ * @returns {Array} new array.
+ */
+const removeFromArray = function(array, ...items) {
+    return array.filter((element) => !items.includes(element));
 };
 
 // Do not edit below this line
