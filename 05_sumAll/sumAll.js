af1f85c431d24897209a9ec71225dbb4c27170b0
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-01-30 19:46:44 +0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+/**
+ * Returns the sum of every number between them (inclusive).
+ * @param {number} start
+ * @param {number} end
+ */
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start)
+        || !Number.isInteger(end) 
+        || start < 0 
+        || end < 0) {
+        return "ERROR";
+    }
 
+    // Swap between start and end
+    if (start > end) {
+        const temp = start;
+        start = end;
+        end = temp;
+    }
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
