863952f185a5f09163e7b53b7c73086b5996c937
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-01-31 11:51:22 +0700

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+/**
+ * Determines whether or not a given year is a leap year.
+ * @param {number} year 
+ */
+const leapYears = function(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 };
 
 // Do not edit below this line
