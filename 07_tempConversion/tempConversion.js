20c555f0200253685a2923ed7a233b69209b4636
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-01-31 12:56:48 +0700

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+/**
+ * Converts fahrenheit to celsius.
+ * @param {number} fahrenheit 
+ * @param {number} [decimalPlace = 1] - rounded to specified decimal place.
+ */
+const convertToCelsius = function(fahrenheit, decimalPlace = 1) {
+  const convertResult = (fahrenheit-32) * (5/9);
+  return parseFloat(convertResult.toFixed(decimalPlace));
 };
 
-const convertToFahrenheit = function() {
+/**
+ * Covert celsius to fahrenheit.
+ * @param {number} celsius 
+ * @param {number} [decimalPlace = 1] - rounded to specified decimal place.
+ */
+const convertToFahrenheit = function(celsius, decimalPlace = 1) {
+  const convertResult = (celsius * (9/5)) + 32;  
+  return parseFloat(convertResult.toFixed(decimalPlace));
 };
 
 // Do not edit below this line
