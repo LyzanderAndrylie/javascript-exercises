aa4ede7303a5087f1f029c8c76e66b21ac5f007e
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-06-25 14:37:42 +0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const number = Number(num);
 
+  if (Number.isNaN(number) || number < 0) {
+    return 'OOPS';
+  }
+
+  if (number === 0) {
+    return number;
+  }
+
+  let prevFib = 0;
+  let currentFib = 1;
+
+  for (let i = 0; i < number - 1; i++) {
+    [prevFib, currentFib] = [currentFib, prevFib+currentFib];
+  }
+
+  return currentFib;
 };
 
 // Do not edit below this line
