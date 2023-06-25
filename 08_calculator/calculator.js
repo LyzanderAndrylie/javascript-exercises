da842dd82b40112678cd1b7a28f394d27775200f
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-06-25 13:49:53 +0700

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(...num) {
+	return num.reduce((total, value) => total + value, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+  if (num.length === 0) {
+    return 0;
+  }
+
+	return num.reduce((total, value) => total - value);
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return add(...numbers);
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
 
+  return numbers.reduce((total, value) => total * value);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+  for (let i = 2; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
