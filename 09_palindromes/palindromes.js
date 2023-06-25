d03453e4bc3ef08fed7e712b409a31720c87937d
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-06-25 14:22:51 +0700

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const filteredText = text.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const reverseText = filteredText.split('').reverse().join('');
+    return filteredText === reverseText;
 };
 
 // Do not edit below this line
