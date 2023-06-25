988f4599d1d1bc4ada60c0fc2df1207195f5c89c
LyzanderAndrylie <lyzanderandrylie@gmail.com>
2023-06-25 15:18:46 +0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  if (people.length === 0) {
+    return {};
+  }
 
+  return people.reduce((oldestPerson, person) => {
+    const oldestPersonAge = getPersonAge(oldestPerson);
+    const personAge = getPersonAge(person);
+
+    return (oldestPersonAge > personAge) ? oldestPerson : person;
+  });
+};
+
+const getPersonAge = (person) => {
+  return (person.yearOfDeath !== undefined)
+    ? person.yearOfDeath - person.yearOfBirth
+    : new Date().getFullYear() - person.yearOfBirth;
 };
 
 // Do not edit below this line
